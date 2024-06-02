cf601878a51bbb3aa5cea1b2abd510bddbb43ffb
luciano <luciano.vidal@trenesargentinos.gob.ar>
2024-06-02 11:41:43 -0300

    enrutamiento

--- src/routers/Routes.tsx
+++ src/routers/Routes.tsx
@@ -2,14 +2,17 @@ import { BrowserRouter, Routes, Route,Navigate  } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import PrivateRoute from './PrivateRoute';
 import Esquemas from '../components/Esquemas'
+import Navbar from "../components/Navbar";
+import PdfViewer from '../components/PdfViewer';
 
-function App() {
+function Routers() {
     return (
         <BrowserRouter>
-            <h1 className="text-4xl font-bold">EsqueMap Linea Roca</h1>
+            <Navbar />
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
                 <Route element={<PrivateRoute />}>
+                    <Route path="/croquis" element={<PdfViewer  fileUrl="../assets/Croquis Velocidades LGR.pdf"/>} />
                     <Route path="/esquemas" element={<Esquemas />} />
                 </Route>
                 <Route path="*" element={<Navigate to="/esquemas" />} />
@@ -18,4 +21,4 @@ function App() {
     );
 }
 
-export default App;
+export default Routers;
